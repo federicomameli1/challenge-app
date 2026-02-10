008a8244fbcbcbf28c5452d88ab28ed2744429f8
bl4ckj4c <jacopo.marino.1998@gmail.com>
2026-02-10 17:14:54 +0100

    Try triggering CI

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
           validate pipeline updates.
         </p>
         <Button
-          className="bg-blue-600 text-white transition active:scale-95 active:shadow-inner hover:bg-blue-700"
+          className="bg-red-600 text-white transition active:scale-95 active:shadow-inner hover:bg-red-700"
           onClick={() => setShowToast(true)}
         >
           Demo Button
